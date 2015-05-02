144b332180cf048510c535f475907818fa8d3430
Jorritisief <jorritbos@home.nl>
2015-05-02 10:21:37 +0200

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,19 @@
+<html>
+
+<title>blog</title>
+<body>
+</body>
+<head> <?php
+include "boven.php";
+?>
+ </head>
+<script></script>
+<?php
+include "blog/index.php";
+?>
+</html>
+
+<?php
+include "onder.php";
+ 
+?>
