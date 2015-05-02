d025e1be88a39ee89854e1aa0958e3430dcb9844
Jorritisief <jorritbos@home.nl>
2015-05-02 10:20:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<html>
+
+<title>jorritisief md style</title>
+<body>
+</body>
+<head> <?php
+include "boven.php";
+?>
+</head>
+<br>
+<br>
+			<div class="box"><h1>henk</h1></div>
+
+<br>
+			<div class="box"><h2>foot</h2>
+</div>
+</html>
+<footer>
+<?php
+include "onder.php";
+ 
+?>
+</footer>
