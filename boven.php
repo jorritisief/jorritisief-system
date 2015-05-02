09a2708b5df52c76de8a4af82b565337ea5e765b
Jorritisief <jorritbos@home.nl>
2015-05-02 10:21:14 +0200

    Create boven.php

---
+++ boven.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+	
+
+<head>
+
+		<link rel="stylesheet" href="style/bootstrap.css" />
+		<link rel="stylesheet" href="style.css" />
+
+		
+
+	</head>
+	
+	<body>
+		
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#"></a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav">
+				<li><a href="index.php">Home</a></li>
+				<li>
+<a href="blog.php">Nieuws</a></li>
+              <li>
+<a href="#">Portfolio</a></li>
+				<li><a href="#">Contact</a></li>
+			  </ul>
+			  
+			</div>
+		  </div>
+		</nav>
+		
+
+		</div>
